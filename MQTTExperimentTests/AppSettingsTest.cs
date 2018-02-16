713ad417d61575a6a306829a23240a1f65e32bcd
ashbygeek <ashbygeek@gmail.com>
2018-02-15 21:21:38 -0500

    Initial Commit: Basic MQTT Client functionality wrappers and application settings framework

---
+++ MQTTExperimentTests/AppSettingsTest.cs
@@ -0,0 +1,66 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MQTTExperiment.Tests
+{
+    [TestClass]
+    public class AppSettingsTest
+    {
+        [TestMethod]
+        public void SaveReadSettingsTest()
+        {
+            // Get the temp file, but make sure it is empty
+            var filePath = PrepareFile();
+
+            // Generate, modify, and save settings
+            var settings = AppSettings.GetSettings(filePath);
+            settings.Name = "Bob";
+            settings.SaveSettings(filePath);
+
+            // Read the settings back
+            var fileContents = File.ReadAllText(filePath);
+            var newSettings = AppSettings.GetSettings(filePath);
+
+            // Verify our settings match
+            Assert.AreEqual(settings.Name, newSettings.Name);
+
+            // Clean up our temp file
+            PrepareFile();
+        }
+
+        [TestMethod]
+        public void PropertyChangedTest()
+        {
+            var propertiesChanged = new List<string>();
+
+            var settings = new AppSettings();
+            settings.PropertyChanged += (s, e) => propertiesChanged.Add(e.PropertyName);
+
+            settings.Name = "New Name";
+
+            Assert.IsTrue(propertiesChanged.Count == 1);
+            Assert.IsTrue(propertiesChanged.Contains(nameof(settings.Name)));
+        }
+
+        [TestMethod]
+        public void DefaultNameTest()
+        {
+            var filePath = PrepareFile();
+            var settings = AppSettings.GetSettings(filePath);
+            Assert.IsTrue(settings.Name.StartsWith("UnNamed-"));
+        }
+
+
+        private string PrepareFile()
+        {
+            var filePath = Path.Combine(Path.GetTempPath(), "MQTTExperiment.Tests.AppSettings.json");
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+            return filePath;
+        }
+    }
+}
